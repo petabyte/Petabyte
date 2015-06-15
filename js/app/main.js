c482cbc6020ef411d0c3fc6dcb389f23920ed446
George Sanchez <petabyte.gs@gmail.com>
2015-06-15 17:56:53 -0500

    Coded navigation

--- js/app/main.js
+++ js/app/main.js
@@ -3,8 +3,33 @@
  */
 $(document).ready(function()
 {
+    var lastLocation = localStorage.getItem('lastLocation');
+    var $navigation = $('.nav.navbar-nav');
+    var $sideMenu = $('#sidemenu');
+    var $indexPage = $('.caption');
+    var $linkNavigation = $navigation.find('li a');
+    var $linkSideMenu = $sideMenu.find('a');
+    var $indexPageNavigation = $indexPage.find('h4 a');
+
+    var setLastLocationTopNav = function(element){
+        var $currentMenuIcon = element.children("i");
+        $currentMenuIcon.addClass("fa-circle");
+    }
+
+    var setLastLocationSideMenu = function(element){
+        element.addClass("active");
+    }
+
+    if (lastLocation){
+        var $currentTopNav = $linkNavigation.filter('[href=\''+lastLocation+'\']');
+        var $currentSideMenu = $linkSideMenu.filter('[href=\''+lastLocation+'\']');
+        setLastLocationTopNav($currentTopNav);
+        setLastLocationSideMenu($currentSideMenu);
+    }
+
+
     //Navigation Menu Toggle
-    $('li a').hover(function(){
+    $navigation.hover(function(){
         var $menuIcon = $(this).children("i");
         $menuIcon.removeClass("fa-circle-o");
         $menuIcon.addClass("fa-dot-circle-o");
@@ -12,6 +37,24 @@ $(document).ready(function()
         var $menuIcon = $(this).children("i");
         $menuIcon.removeClass("fa-dot-circle-o");
         $menuIcon.addClass("fa-circle-o");
+    });
+
+    $linkSideMenu.click(function (e){
+        $(this).removeClass("active");
+        setLastLocationSideMenu($(this));
+        localStorage.setItem('lastLocation',$(this).attr("href"));
+
+    });
+
+    $linkNavigation.click(function(e){
+        var $menuIcon = $(this).children("i");
+        $menuIcon.removeClass("fa-circle");
+        setLastLocationTopNav($(this))
+        localStorage.setItem('lastLocation',$(this).attr("href"));
+    });
+
+    $indexPageNavigation.click(function(e){
+        localStorage.setItem('lastLocation',$(this).attr("href"));
     });
 
 });
