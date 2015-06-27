50aacd5d280791dd34efd128676ef9c839a0d23d
George Sanchez <petabyte.gs@gmail.com>
2015-06-26 23:08:07 -0500

    HTML5 Validation was gone when handling the form submit with jquery

--- js/app/contact.js
+++ js/app/contact.js
@@ -2,8 +2,23 @@
  * Created by George on 6/26/2015.
  */
 $(document).ready(function(){
+    var isValid = null;
     var $submitButton = $("button");
+    var $form = $("form");
+    var submitForm = function(e){
+        e. preventDefault();
+        if(isValid === false){
+            isValid = null;
+            return false;
+        }
+        $submitButton.trigger("click");
+    };
+    $form.submit(submitForm);
     $submitButton.on("click", function(event){
+        isValid = $form[0].checkValidity();
+        if(false ===  isValid){
+            return true;
+        }
         event.preventDefault();
         var $name = $("#name");
         var $email = $("#email");
